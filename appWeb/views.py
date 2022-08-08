f008708b9ead89e07d59653f40e7db5b910dbb48
AFE <afe95xd@gmail.com>
2022-08-07 21:30:16 -0500

    Modifique el views

--- appWeb/views.py
+++ appWeb/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def saludo(request):
-    return HttpResponse("Hola esta es mi primera aplicacion django")
+    return HttpResponse("Hola esta es mi primera aplicacion django proxima a mejorar y espero que no salgan errores :) ")
